86d5f761ec81316b9d5f8687995dd7c5d7a3ab8e
ruslan5665 <ruslan5665@gmail.com>
2017-03-03 10:53:59 +0200

    Revert "Revert "Lesson 2""
    This reverts commit ecdc434a87fcfe1913b9cefb15c1cb4befb6d281.

---
+++ Lesson 2/Sqrt.java
@@ -0,0 +1,17 @@
+﻿package com.gmail.ruslan5665;
+
+public class Sqrt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double a = 0.3;
+		double b = 0.4;
+		double c = 0.5;
+		
+		double p = 0.5*(a+b+c);
+		
+		System.out.println("S = "+Math.sqrt(p*(p-a)*(p-b)*(p-c)));
+	}
+
+}
